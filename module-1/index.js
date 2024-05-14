336f079af71dd5f766c90fe26f3baf3522989253
Dolon Chandra Roy <dolonr718@gmail.com>
2024-05-14 20:09:08 +0600

    read and write file with node js commit

--- module-1/index.js
+++ module-1/index.js
@@ -1,7 +1,10 @@
 
 
+// module synchronous vs asynchronous
 const fs = require("fs");
 
-const readText = fs.readFileSync("./read.txt");
+const readText = fs.readFileSync("./read.txt", "utf-8"); // for writing file
 console.log(readText);
 
+const writeText = fs.writeFileSync("./write.text", readText + "some extra");
+console.log(writeText)
