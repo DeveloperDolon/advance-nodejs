b43b3b231324c21473372126d7fc51fce38fce97
Dolon Chandra Roy <dolonr718@gmail.com>
2024-05-14 20:22:49 +0600

    asynchronous and synchronous method learning commit

--- module-1/index.js
+++ module-1/index.js
@@ -3,8 +3,24 @@
 // module synchronous vs asynchronous
 const fs = require("fs");
 
-const readText = fs.readFileSync("./read.txt", "utf-8"); // for writing file
-console.log(readText);
+// const readText = fs.readFileSync("./read.txt", "utf-8"); // for writing file synchronous way
+// console.log(readText);
 
-const writeText = fs.writeFileSync("./write.text", readText + "some extra");
-console.log(writeText)
+// const writeText = fs.writeFileSync("./write.text", readText + "some extra");
+// console.log(writeText)
+
+//  asynchronous way
+fs.readFile("./read.txt", "utf-8", (err, data) => {
+    if(err) {
+        throw Error("Error reading text");
+    }
+    console.log(data);
+
+    fs.writeFile("./write.text", data, "utf-8", (err) => {
+        if(err) {
+            throw Error("Error to write text");
+        }
+    } )
+});
+
+console.log("testing the practise");
